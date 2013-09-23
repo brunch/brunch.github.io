f092e95bcb6a3d5e2fecd56d7b9ab4f0bb9c56b4
Paul Miller <paul@paulmillr.com>
2013-09-23 17:43:43 +0300

    Fix styles, remove own page for examples.

--- javascripts/main.js
+++ javascripts/main.js
@@ -86,9 +86,15 @@ $(document).ready(function(){
     links.forEach(function(link) {
       link.className = link.className.replace('active', '');
       var re = new RegExp(link.getAttribute('data-pattern'));
-      console.log(location.href, re);
       if (re.test(location.href)) link.className += ' active';
     });
+    document.querySelector('.page-nav .toggle-navigation').addEventListener('click', function() {
+      queryAll('.page-nav .nav-toggle-content').forEach(function(item) {
+        var existing = window.getComputedStyle(item).display;
+        var newStyle = (existing === 'none') ? 'list-item' : 'none';
+        item.style.display = newStyle;
+      });
+    }, true);
   })();
 });
 
