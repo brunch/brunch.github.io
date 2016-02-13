1c005db2114811a69705aba1c64584db8515e732
Paul Miller <paul@paulmillr.com>
2016-02-13 05:18:41 +0200

    Update and rename config.coffee to brunch-config.js

---
+++ source/brunch-config.js
@@ -0,0 +1,41 @@
+module.exports = {
+  files: {
+    javascripts: {joinTo: 'scripts/app.js'},
+    templates: {joinTo: {
+      'scripts/app.js': /^app\/views/,
+      'scripts/static.js': /^app\/.*.jade$/
+    }}
+  },
+  paths: {public: '..'},
+  plugins: {
+    jade: {
+      locals: {
+        nav: [
+          {title: 'Home', path: ''},
+          {title: 'Docs', path: 'https://github.com/brunch/brunch/tree/master/docs'},
+          {title: 'Guide', path: 'https://github.com/brunch/brunch-guide#readme'},
+          {title: 'Plugins', path: 'plugins'},
+          {title: 'Skeletons', path: 'skeletons'},
+          {title: 'Examples', path: 'examples'},
+          {title: 'Compare', path: 'compare'}
+        ],
+        social: [
+          {
+            classname: 'github',
+            width: 160,
+            src: 'http://ghbtns.com/github-btn.html?user=brunch&repo=brunch&type=watch&count=true&size=large'
+          }, {
+            classname: 'twitter',
+            width: 260,
+            src: 'https://platform.twitter.com/widgets/follow_button.html?screen_name=brunch&show_count=true&size=l'
+          }, {
+            classname: 'twitter',
+            width: 130,
+            src: 'http://platform.twitter.com/widgets/tweet_button.html?count=horizontal&id=twitter-widget-0&lang=en&original_referer=http%3A%2F%2Flocalhost%3A3333%2F&related=brunch&size=l&text=Brunch%20%7C%20HTML5%20application%20assembler&url=http%3A%2F%2Fbrunch.io&via=brunch" class="twitter-share-button twitter-tweet-button twitter-count-horizontal'
+          }
+        ]
+      }
+    },
+    autoReload: {enabled: {css: true, js: false, assets: true}}
+  }
+};
