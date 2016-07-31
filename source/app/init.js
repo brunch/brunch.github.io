3ea149a70172c1552267498de1e5c6cbd8a0f7ca
denar90 <denysov.artem@gmail.com>
2016-08-01 00:23:11 +0300

    add offline support using service worker

--- source/app/init.js
+++ source/app/init.js
@@ -55,6 +55,11 @@ function setTitle(contentClass) {
   document.title = heading.textContent + ' — Brunch';
 }
 
+function registerSW() {
+  if (navigator.serviceWorker) navigator.serviceWorker.register('/sw.js');
+}
+
 setTitle('page__content');
 toc('doc-content', 'page__content', 'toc-placeholder');
 anchorize();
+registerSW();
