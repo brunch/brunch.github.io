a25c8913d1353e440000044a1d8429e20f3bb449
Denys Dovhan <email@denysdovhan.com>
2016-12-30 21:33:04 +0200

    Few fixes and useful comment in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
+
+# Force a pipeline to produce a failure return code if any command errors
 set -o pipefail
 
 echo "$TRAVIS_BRANCH"
 
-if [ "$TRAVIS_BRANCH" = "source" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]
-then
+if [ "$TRAVIS_BRANCH" == "source" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   echo "Deploying!"
   npm run prod
   cd public
@@ -12,7 +13,7 @@ then
   git config --global user.name "TRAVIS-CI"
   git init
   git add .
-  git commit -m "deploy"
+  git commit -m "Deploy at $(date)"
   git push --force "https://${GH_TOKEN}@github.com/brunch/brunch.github.io.git" master
 else
   npm run prod
