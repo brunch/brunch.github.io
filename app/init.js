961522687d65b705c38600ae3e78a791e5a6a797
Denys Dovhan <email@denysdovhan.com>
2017-01-09 17:35:20 +0200

    Fix broken routing in development mode.  (#234)

--- app/init.js
+++ app/init.js
@@ -73,6 +73,15 @@ function deferIframe() {
   });
 }
 
+// Just a small hack
+// Add `.html` extansion for each of internal links
+// Fix for: https://github.com/brunch/brunch.github.io/issues/233
+if (process.env.NODE_ENV === 'development') {
+  [...document.querySelectorAll('a[href]')]
+    .filter(({ pathname }) => pathname !== '/' && pathname.startsWith('/'))
+    .forEach(link => link.href += '.html');
+}
+
 setTitle('page__content');
 toc('doc-content', 'page__content', 'toc-placeholder');
 anchorize();
