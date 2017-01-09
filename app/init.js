9b1c07601a2c2b32eb8721a58b2372884987fa65
Denys Dovhan <email@denysdovhan.com>
2017-01-09 17:33:38 +0200

    Simplify a solution of #233

--- app/init.js
+++ app/init.js
@@ -78,14 +78,8 @@ function deferIframe() {
 // Fix for: https://github.com/brunch/brunch.github.io/issues/233
 if (process.env.NODE_ENV === 'development') {
   [...document.querySelectorAll('a[href]')]
-    .filter(link => {
-      const href = link.getAttribute('href');
-      return href !== '/' && href.startsWith('/');
-    })
-    .forEach(link => {
-      const href = link.getAttribute('href');
-      link.setAttribute('href', `${href}.html`);
-    });
+    .filter(({ pathname }) => pathname !== '/' && pathname.startsWith('/'))
+    .forEach(link => link.href += '.html');
 }
 
 setTitle('page__content');
