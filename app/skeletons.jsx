58d86076ec20f39deda810bdfd964b11b7281c6d
Denys Dovhan <email@denysdovhan.com>
2017-01-11 12:45:07 +0200

    Rewrite skeletons.jsx in ES2015

--- app/skeletons.jsx
+++ app/skeletons.jsx
@@ -1,64 +1,80 @@
+/* eslint no-extra-parens: 0 */
+'use strict';
+
 require('whatwg-fetch');
-var filterItems = require('./utils').filterItems;
+const React = require('react');
+const {filterItems} = require('./utils');
 
 var Body = React.createClass({
-  getInitialState: function() {
+  getInitialState() {
     return {
       skeletons: [],
-      search: ''
+      search: '',
     };
   },
 
-  componentWillMount: function() {
-    var self = this;
-
+  componentWillMount() {
     fetch('https://raw.githubusercontent.com/brunch/skeletons/master/skeletons.json')
-      .then(function(x) { return x.json(); })
-      .then(function(x) {
-        self.setState({ skeletons: x.skeletons });
-      });
+      .then(res => res.json())
+      .then(({skeletons}) => this.setState({skeletons}));
   },
 
-  handleKeyUp: function(e) {
-    this.setState({ search: e.target.value });
+  handleKeyUp(e) {
+    this.setState({search: e.target.value});
   },
 
-  filteredSkeletons: function() {
-    var skeletons = this.state.skeletons;
-    var search = this.state.search;
-    return filterItems(skeletons, search, ['name', 'url', 'alias', 'technologies', 'description']);
+  filteredSkeletons() {
+    const {skeletons, search} = this.state;
+    return filterItems(skeletons, search, [
+      'name', 'url', 'alias', 'technologies', 'description',
+    ]);
   },
 
-  render: function() {
-    var skeletonItems = this.filteredSkeletons().map(function(skeleton, i) {
-      var fullURL = "https://github.com/" + skeleton.url;
-
-      return <tr key={i}>
-        <td><a href={fullURL} target="_blank">{skeleton.title}</a></td>
-        <td><code>{skeleton.url}</code></td>
-        <td><code>{skeleton.alias || '-'}</code></td>
-        <td>{skeleton.technologies}</td>
+  render() {
+    const skeletonItems = this.filteredSkeletons().map((skeleton, i) => (
+      <tr key={i}>
+        <td>
+          <a href={`https://github.com/${skeleton.url}`} target="_blank">
+            {skeleton.title}
+          </a>
+        </td>
+        <td>
+          <code>{skeleton.url}</code>
+        </td>
+        <td>
+          <code>{skeleton.alias || '-'}</code>
+        </td>
+        <td>
+          {skeleton.technologies}
+        </td>
         <td dangerouslySetInnerHTML={{__html: skeleton.description}} />
-      </tr>;
-    });
-    return <div>
-      <input placeholder="Type to search... It could be a technology name or anything, really" type="text" className="searchbox" onKeyUp={this.handleKeyUp}/>
-      <table className="data-table">
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>URL</th>
-            <th>Alias</th>
-            <th>Technologies</th>
-            <th>Description</th>
-          </tr>
-        </thead>
-        <tbody>
-          {skeletonItems}
-        </tbody>
-      </table>
-    </div>;
-  }
+      </tr>
+    ));
+
+    return (
+      <div>
+        <input
+          placeholder="Type to search... It could be a technology name or anything, really"
+          type="text"
+          className="searchbox"
+          onKeyUp={this.handleKeyUp} />
+        <table className="data-table">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>URL</th>
+              <th>Alias</th>
+              <th>Technologies</th>
+              <th>Description</th>
+            </tr>
+          </thead>
+          <tbody>
+            {skeletonItems}
+          </tbody>
+        </table>
+      </div>
+    );
+  },
 });
 
 module.exports = Body;
